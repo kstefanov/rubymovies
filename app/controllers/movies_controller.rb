7554ab611890a72a558f1166a47b61754964a739
Kristiyan <kristiqn73@gmail.com>
2017-12-17 12:51:24 +0000

    exercise 9

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,26 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.all
+  sort = params[:sort] || session[:sort]
+  case sort
+  when 'title'
+    ordering,@title_header = {:title => :asc}, 'hilite'
+  when 'release_date'
+    ordering,@date_header = {:release_date => :asc}, 'hilite'
   end
+  @all_ratings = Movie.all_ratings
+  @selected_ratings = params[:ratings] || session[:ratings] || {}
+    
+  if @selected_ratings == {}
+    @selected_ratings = Hash[@all_ratings.map {|rating| [rating, rating]}]
+  end
+    
+  if params[:sort] != session[:sort] or params[:ratings] != session[:ratings]
+    session[:sort] = sort
+    session[:ratings] = @selected_ratings
+    redirect_to :sort => sort, :ratings => @selected_ratings and return
+  end
+  @movies = Movie.where(rating: @selected_ratings.keys).order(ordering)
+end
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
@@ -46,5 +65,19 @@ def destroy
  flash[:notice] = "Movie '#{@movie.title}' deleted."
  redirect_to movies_path
 end
+sort = params[:sort]
+case sort
+when 'title'
+ ordering,@title_header = {:title => :asc}, 'hilite'
+when 'release_date'
+ ordering,@date_header = {:release_date => :asc}, 'hilite'
+end
+@movies = Movie.order(ordering)
+@all_ratings = Movie.all_ratings
+@selected_ratings = params[:ratings]|| {}
+if @selected_ratings == {}
+ @selected_ratings = Hash[@all_ratings.map {|rating| [rating, rating]}]
+end
+@movies = Movie.where(rating: @selected_ratings.keys).order(ordering)
 
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -14,4 +14,6 @@ class Movie < ActiveRecord::Base
  def grandfathered?
  release_date && release_date < @@grandfathered_date
  end
+ def self.all_ratings ; %w[G PG PG-13 R NC-17] ; end
+
 end
